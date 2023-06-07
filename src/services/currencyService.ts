58bee6b792132ac56f2f7ba13acc8391c2863fcf
Thomas Albuquerque <tealbthomas@gmail.com>
2023-06-07 15:41:52 -0300

    Feito e testado armazenamento de currencyList em cookie.

--- src/services/currencyService.ts
+++ src/services/currencyService.ts
@@ -11,64 +11,81 @@ export interface Currency {
 }
 
 function processCurrencyList(currencyList: any) {
-  const rawCl: { code: string, value: number }[] = Object.values(currencyList['data'])
+  const rawCl: { code: string; value: number }[] = Object.values(currencyList['data'])
   let newCL: Currency[] = []
   rawCl.forEach((rawCurrency, index) => {
-    newCL.push({ id: index, currencyName: rawCurrency.code, ratioPerDollar: rawCurrency.value.toString(), createdAt: new Date() })
+    newCL.push({
+      id: index,
+      currencyName: rawCurrency.code,
+      ratioPerDollar: rawCurrency.value.toString(),
+      createdAt: new Date(),
+    })
   })
   return newCL
 }
 
 async function fetchCurrencyList() {
-  const response = await CurrencyAPI() //fetchTestApi()
+  const response = await CurrencyAPI()  //fetchTestApi()
   const currencyList = processCurrencyList(response)
   return currencyList
 }
-function setLocalStore(currencyList: Currency[], dateString: string) {
-  const stored = {
-    currencyList: currencyList,
-    dateString: dateString
-  };
-  localStorage.setItem('storedCurrencyList', JSON.stringify(stored));
+
+function setCookie(name: string, value: string, days: number) {
+  const expirationDate = new Date()
+  // expirationDate.setTime(expirationDate.getTime() + days * 24 * 60 * 60 * 1000)
+  expirationDate.setTime(expirationDate.getTime() + 25000)
+  const expires = "expires=" + expirationDate.toUTCString()
+  document.cookie = name + "=" + value + ";" + expires + ";path=/"
+}
+
+function getCookie(name: string) {
+  const decodedCookies = decodeURIComponent(document.cookie)
+  const cookies = decodedCookies.split(';')
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim()
+    if (cookie.indexOf(name + "=") === 0) {
+      return cookie.substring(name.length + 1)
+    }
+  }
+  return ""
 }
 
 const currencyService = {
   getCurrenciesExternal: async function () {
-
-    const stored_stringFormat = localStorage.getItem('storedCurrencyList');
-    const today = new Date().toLocaleDateString();
+    const stored_stringFormat = getCookie('storedCurrencyList')
+    const today = new Date().toLocaleDateString()
 
     if (stored_stringFormat) {
-      const stored: { currencyList: Currency[], dateString: string } = JSON.parse(stored_stringFormat);
+      const stored: { currencyList: Currency[]; dateString: string } = JSON.parse(stored_stringFormat)
       if (stored.dateString !== today) {
         try {
           const currencyList = await fetchCurrencyList()
-          setLocalStore(currencyList, today)
+          setCookie('storedCurrencyList', JSON.stringify({ currencyList, dateString: today }), 1)
           return currencyList
         } catch (error) {
-          return [];
+          return []
         }
       } else {
-        return (stored.currencyList)
+        return stored.currencyList
       }
     } else {
       try {
         const currencyList = await fetchCurrencyList()
-        setLocalStore(currencyList, today)
-        return currencyList;
+        setCookie('storedCurrencyList', JSON.stringify({ currencyList, dateString: today }), 1)
+        return currencyList
       } catch (error) {
-        return [];
+        return []
       }
     }
   },
   getCurrencies: async function () {
     const res: AxiosResponse = await api.get('/currencies').catch((error) => {
-      return error.response;
-    });
+      return error.response
+    })
 
-    const typedRes: Currency[] = res.data;
-    return typedRes;
-  }
+    const typedRes: Currency[] = res.data
+    return typedRes
+  },
 }
 
 export default currencyService
