a06d66c970bb0170259b26a8639de9fe2b25fab0
Thomas Albuquerque <tealbthomas@gmail.com>
2023-06-09 18:37:38 -0300

    Feita a responsividade do Toast.

--- src/components/common/toast/index.tsx
+++ src/components/common/toast/index.tsx
@@ -1,4 +1,5 @@
-import { Toast, ToastBody } from "reactstrap";
+import { Toast, ToastBody } from 'reactstrap';
+import styles from './styles.module.scss';
 
 interface props {
   isOpen: boolean;
@@ -10,10 +11,11 @@ const ToastComponent = function ({ isOpen, message, color }: props) {
   return (
     <>
       <Toast
-        className={`${color} text-white fixed-top ms-auto mt-3`}
-        isOpen={isOpen}
-      >
-        <ToastBody className="text-center">{message}</ToastBody>
+        className={`${color} text-white fixed-top ms-auto mt-3 ${styles.toast}`}
+        isOpen={isOpen}>
+        <ToastBody className={`text-center ${styles.toastBody}`}>
+          {message}
+        </ToastBody>
       </Toast>
     </>
   );
